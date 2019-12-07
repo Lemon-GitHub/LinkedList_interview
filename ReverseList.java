cfe7501bcc49aef51d437cddf31801d15bd6103e
liu <1373788543@qq.com>
2019-12-07 22:37:27 +0800

    链表牛客面试题

---
+++ ReverseList.java
@@ -0,0 +1,41 @@
+package LinkedList_interview;
+
+public class ReverseList {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode ReverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = null;
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            if (next == null) {
+                newHead = cur;
+            }
+
+            // 翻转
+            cur.next = prev;
+
+            // 更新
+            prev = cur;
+            cur = next;
+        }
+
+        return newHead;
+    }
+}
